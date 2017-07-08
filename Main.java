45f652aedd9c687ad9ab346103298e3013428c8b
HristoMalakov <hr.malakov@gmail.com>
2017-07-08 19:20:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame JFobj = new JFrame();
+		Gameplay GMobj = new Gameplay();
+		JFobj.setBounds(0,0,700,600);
+		JFobj.setResizable(false);
+		JFobj.setVisible(true);
+		JFobj.setTitle("Brick Breaker JFX90");
+		JFobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFobj.add(GMobj);
+
+
+	}
+
+}
---
+++ MapGenerator.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MapGenerator{
+
+	public int map [] [];
+	public int brickWidth;
+	public int brickHeight;
+	public MapGenerator(int row, int col){
+		//creates the bricks
+		map = new int [row] [col];
+		for(int i = 0; i < map.length; i++){
+			for(int j = 0; j < map[0].length; j++){
+				//brick = 1 means brick exists, brick = 0 = brick has been broken
+				map [i] [j] = 1;
+			}
+		}
+		//set how many bricks we would create
+		brickWidth = 540/col;
+		brickHeight = 150/row;	
+	}
+	//draws the bricks
+	public void draw(Graphics2D g){
+		
+		for(int i = 0; i < map.length; i++){
+			for(int j = 0; j < map[0].length; j++){
+				if(map [i] [j] > 0){
+					
+					//fills the bricks
+					g.setColor(Color.WHITE);
+					g.fillRect(j * brickWidth + 80, i* brickHeight + 50, 
+							brickWidth, brickHeight);
+					
+					//creating borders for our bricks
+					g.setStroke ( new BasicStroke( 3 ) );
+					g.setColor(Color.BLACK);
+					g.drawRect( j * brickWidth + 80, i* brickHeight + 50, 
+							brickWidth, brickHeight );
+				}				
+			}
+		}		
+	}
+	//sets the brick value to 0, meaning it's broken
+	public void setBrickValue(int value, int row, int col){		
+		map [row] [col] = value;	
+	}
+}
